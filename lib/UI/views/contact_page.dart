984681cb70b7a153a084a693a2d31b6c5c6bf24a
Moin97 <moinuddin9777@gmail.com>
2023-01-09 16:33:01 +0530

    added contact page

---
+++ lib/UI/views/contact_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class ContactPage extends StatefulWidget {
+  const ContactPage({Key? key}) : super(key: key);
+
+  @override
+  State<ContactPage> createState() => _ContactPageState();
+}
+
+class _ContactPageState extends State<ContactPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Center(
+          child: Text(
+            "CONTACT PAGE UNDER DEV",
+            style: Theme.of(context).textTheme.headline2,
+          ),
+        )
+      ]),
+    );
+  }
+}
