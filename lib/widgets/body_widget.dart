e8ad650d01bca9514ec24a15e3146203383cc369
Moin97 <moinuddin9777@gmail.com>
2023-01-09 16:27:35 +0530

    code refactor

---
+++ lib/widgets/body_widget.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Text(
+            "Hey!, Welcome to this site",
+            style: Theme.of(context).textTheme.headline2!.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+          const Text(
+            "This is a simple single page UI of a web page made with flutter",
+            style: TextStyle(
+              fontSize: 21,
+              // color: kTextcolor.withOpacity(0.34),
+            ),
+          ),
+          FittedBox(
+            // Now it just take the required spaces
+            child: Container(
+              margin: EdgeInsets.symmetric(vertical: 20),
+              padding: EdgeInsets.all(15),
+              decoration: BoxDecoration(
+                color: Color(0xFF372930),
+                borderRadius: BorderRadius.circular(34),
+              ),
+              child: Row(
+                children: <Widget>[
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    height: 38,
+                    width: 38,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                    ),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Color(0xFF372930),
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 15),
+                  Text(
+                    "Get Started".toUpperCase(),
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 19,
+                    ),
+                  ),
+                  SizedBox(width: 15),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
