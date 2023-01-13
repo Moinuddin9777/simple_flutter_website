720a19c64285d5c8f92a11eae9b40a311b074883
Moin97 <moinuddin9777@gmail.com>
2023-01-13 16:15:19 +0530

    updated page

--- lib/UI/views/about_page.dart
+++ lib/UI/views/about_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:simple_flutter_website/themes.dart';
+import 'package:simple_flutter_website/widgets/custom_app_bar.dart';
 
 class AboutPage extends StatefulWidget {
   const AboutPage({Key? key}) : super(key: key);
@@ -11,11 +13,12 @@ class _AboutPageState extends State<AboutPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+      body: Column(mainAxisAlignment: MainAxisAlignment.start, children: [
+        CustomAppBar(),
         Center(
           child: Text(
             "ABOUT PAGE UNDER DEV",
-            style: Theme.of(context).textTheme.headline2,
+            style: headlineTextStyle,
           ),
         )
       ]),
