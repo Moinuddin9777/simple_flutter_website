243e10ef253c653b162542981f27895613a24ef4
Moin97 <moinuddin9777@gmail.com>
2023-01-17 16:26:46 +0530

    updated API endpoint

---
+++ lib/utils/constants.dart
@@ -0,0 +1,4 @@
+class AppConstants {
+  static const placeholderApiUrl = 'https://jsonplaceholder.typicode.com';
+  static const productsApiUrl = 'http://makeup-api.herokuapp.com/api/v1/products.json' ;
+}
