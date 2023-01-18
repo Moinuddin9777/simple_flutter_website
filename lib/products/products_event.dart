521419378ac664853d86020bd2e89cf82bdfbbd9
Moin97 <moinuddin9777@gmail.com>
2023-01-18 12:01:59 +0530

    added event

---
+++ lib/products/products_event.dart
@@ -0,0 +1,9 @@
+// as we are just displaying data we may not have many events
+// soo here iam creating one event which can be used as per our wish
+// maybe for a button click to fetch data
+
+//import 'package:equatable/equatable.dart';
+
+class ProductsEvent {}
+
+class LoadProductsEvent extends ProductsEvent {}
