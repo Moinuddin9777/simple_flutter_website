fceb96ed3e33d01b0bbad04ac5efba13a123bf03
Moin97 <moinuddin9777@gmail.com>
2023-01-19 11:18:13 +0530

    added theme event

---
+++ lib/bloc/theme/theme_event.dart
@@ -0,0 +1,7 @@
+abstract class ThemeDataEvent {}
+
+class ChangToDarkThemeData extends ThemeDataEvent {}
+
+class ChangToLightThemeData extends ThemeDataEvent {}
+
+class CheckThemeActivated extends ThemeDataEvent {}
