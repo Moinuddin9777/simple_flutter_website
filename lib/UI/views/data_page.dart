d168589a49e74b4a738d39c64a0de46c75fbbb1f
Moin97 <moinuddin9777@gmail.com>
2023-02-14 10:54:07 +0530

    modified page

--- lib/UI/views/data_page.dart
+++ lib/UI/views/data_page.dart
@@ -16,49 +16,54 @@ class DataPage extends StatefulWidget {
 class _DataPageState extends State<DataPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: BlocBuilder<ProductsBloc, ProductsState>(
-        builder: (context, state){
-          if(state is ProductsLoadingState){
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-          if(state is ProductsLoadedState){
-            BlocProvider.value(value:
-            BlocProvider.of<ProductsBloc>(context), child: Card());
-              return Column(
-                children: [
-                  CustomAppBar(),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 26.0),
-                    child: ListView.builder(
-                      // gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      //   crossAxisCount: 3
-                      // ),
-                        shrinkWrap: true,
-                        primary: true,
-                        itemBuilder:
-                      (context, index){
+    return Scaffold(body: BlocBuilder<ProductsBloc, ProductsState>(
+      builder: (context, state) {
+        if (state is ProductsLoadingState) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+        if (state is ProductsLoadedState) {
+          BlocProvider.value(
+              value: BlocProvider.of<ProductsBloc>(context),
+              child: const Card(
+                child:
+                    Center(child: Text('.............DATA LOADED............')),
+              ));
+          return Column(
+            children: [
+              CustomAppBar(),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 26.0),
+                child: ListView.builder(
+                    // gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    //   crossAxisCount: 3
+                    // ),
+                    shrinkWrap: true,
+                    primary: true,
+                    itemBuilder: (context, index) {
                       return Card(
                         child: ListTile(
                           leading: Padding(
                             padding: const EdgeInsets.all(8.0),
-                            child: Container( height: 60, width: 60, child: Image.network(state.products[index].imageLink.toString()) ),
-                          ) ,
-                          title: Text( state.products[index].name.toString()),
-
+                            child: SizedBox(
+                                height: 60,
+                                width: 60,
+                                child: Image.network(state
+                                    .products[index].imageLink
+                                    .toString())),
+                          ),
+                          title: Text(state.products[index].name.toString()),
                         ),
                       );
-            },
+                    },
                     itemCount: state.products.length),
-                  ),
-                ],
-              );
-          }
-          return Container();
-        },
-      )
-    );
+              ),
+            ],
+          );
+        }
+        return Container();
+      },
+    ));
   }
 }
