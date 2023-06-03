c8cdd8f930a060e8022d2eb27917892aad98f875
Moin97 <moinuddin9777@gmail.com>
2023-06-03 11:50:31 +0530

    added api url

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,4 +1,5 @@
 class AppConstants {
   static const placeholderApiUrl = 'https://jsonplaceholder.typicode.com';
-  static const productsApiUrl = 'http://makeup-api.herokuapp.com/api/v1/products.json' ;
+  static const makeupAPI = 'http://makeup-api.herokuapp.com/api/v1/';
+  static const pokeDexApiUrl = 'https://pokeapi.co/api/v2/';
 }
