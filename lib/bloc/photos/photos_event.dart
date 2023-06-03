3773ff24eca1f950675de8ec3906c5928c0b6d8d
Moin97 <moinuddin9777@gmail.com>
2023-06-03 11:47:39 +0530

    removed photos bloc

--- lib/bloc/photos/photos_event.dart
+++
@@ -1,9 +0,0 @@
-// as we are just displayin data we may not have many events
-// soo here iam creating one event which can be used as per our wish
-// maybe for a button click to fetch data
-
-//import 'package:equatable/equatable.dart';
-
-class PhotosEvent {}
-
-class LoadPhotosEvent extends PhotosEvent {}
