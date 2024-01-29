1c3c481f49762665cf41482c42026b08860e3d32
Badavathlokesh <141541441+Badavathlokesh@users.noreply.github.com>
2024-01-29 22:57:02 +0530

    Delete lib directory

--- lib/bloc/theme/theme_event.dart
+++
@@ -1,7 +0,0 @@
-abstract class ThemeDataEvent {}
-
-class ChangToDarkThemeData extends ThemeDataEvent {}
-
-class ChangToLightThemeData extends ThemeDataEvent {}
-
-class CheckThemeActivated extends ThemeDataEvent {}
