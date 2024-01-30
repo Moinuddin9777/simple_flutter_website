7096e65fc482ed24fc1b3da763d387f7ca6ae647
Thanoj <126764120+thanu2932@users.noreply.github.com>
2024-01-30 14:41:00 +0530

    Update product_services.dart

--- lib/services/product_services.dart
+++ lib/services/product_services.dart
@@ -1,27 +1,18 @@
-import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shopx/models/product.dart';
 
-import 'package:dio/dio.dart';
-import 'package:get/get.dart';
-import 'package:simple_flutter_website/core/service_helper.dart';
-import 'package:simple_flutter_website/models/products/products_response.dart';
-import 'package:simple_flutter_website/utils/constants.dart';
+class RemoteServices {
+  static var client = http.Client();
 
-class ProductServices {
-  static Future<ProductsResponse> fetchAllProducts() async {
-    try {
-      var dio = await getDio();
-      var response = await dio.get(
-        '${AppConstants.makeupAPI}products.json',
-      );
-      return ProductsResponse(
-          success: true,
-          message: "success",
-          productData: ProductData.fromJson(response.data));
-    } on DioError catch (e) {
-      logger.printError(
-        info: e.response!.data["message"],
-      );
-      return ProductsResponse(success: false, message: "failed");
+  static Future<List<Product>> fetchProducts() async {
+    var response = await client.get(Uri.parse(
+        'https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline'));
+    if (response.statusCode == 200) {
+      var jsonString = response.body;
+      return productFromJson(jsonString);
+    } else {
+      //show error message
+      return null;
     }
   }
 }
